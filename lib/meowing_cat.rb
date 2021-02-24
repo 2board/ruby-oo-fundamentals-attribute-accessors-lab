f142931bae514b6e126cab4f99eac518e85efec4
David Jun <David>
2021-02-24 11:23:30 -0500

    commit tree f1e59431eecc60e817ff453b96c33d288907ae0e

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,1 +1,19 @@
 ## code your solution here. 
+class Cat
+    def initialize
+        @name = name
+    end
+
+    def name
+        @name
+    end
+    attr_accessor :name
+    
+    def meow
+        puts "meow!"
+    end
+
+    
+ 
+ 
+end
